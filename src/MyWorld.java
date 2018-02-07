3aa202b38c90ede18eb0a9f2b77b1bab44f08cf2
dg12220 <gatadesp100@gmail.com>
2018-02-07 18:06:30 +0530

    First commit

---
+++ src/MyWorld.java
@@ -0,0 +1,13 @@
+
+public class MyWorld {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is My World Project");
+
+		System.out.println("Developed in Tech Mahindra");
+
+	}
+
+}
